52d539b36d3a5f5a6cdc51f7e964158170c75000
Maros Zatko <hacxman@gmail.com>
2013-12-01 03:04:23 +0100

    skipframes: make it tunable

--- skipframes.sh
+++ skipframes.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+if (( $# < 2 )) ; then
+  echo "usage: $0 target_dir count_to_skip"
+  exit 1
+fi
 I=0
 for F in jpgs/*.jpg ; do
   if (( $I == 0 )) ; then
-    echo cp $F kokot/
-    cp "$F" kokot/
+    echo cp $F "$1/"
+    cp "$F" "$1/"
   fi
   I=$((I+1))
-  if (( $I == 4 )) ; then
-  #if [ $I -eq 12 ] ; then
+  if (( $I == $2 )) ; then
     I=0
     continue
   fi
