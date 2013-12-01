4456636019445cb179c11235e70270c0b70d75f7
Maros Zatko <hacxman@gmail.com>
2013-12-01 03:47:58 +0100

    skipframes: be less verbose

--- skipframes.sh
+++ skipframes.sh
@@ -4,14 +4,18 @@ if (( $# < 2 )) ; then
   exit 1
 fi
 I=0
+II=0
+C=$(ls jpgs/*.jpg | wc -l)
 for F in jpgs/*.jpg ; do
   if (( $I == 0 )) ; then
-    echo cp $F "$1/"
+    echo -n -e cp $F "$1/" , "$II of $C\r"
     cp "$F" "$1/"
   fi
   I=$((I+1))
+  II=$((II+1))
   if (( $I == $2 )) ; then
     I=0
     continue
   fi
 done
+echo
